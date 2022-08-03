1dd097d369e223ab093d6b049baf32b67c0ca40f
nitipon <nitipon.apaisri@gmail.com>
2022-08-03 21:46:17 +0200

    Minor fixed and able to fetch data from api by axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import WTChecker from "./pages/WTChecker";
 import "./styles/styles.css";
 function App() {
     return (
         <div className="App">
-            <WTChecker />
+            <Routes>
+                <Route path="/" element={<WTChecker />} />
+            </Routes>
         </div>
     );
 }
--- src/pages/WTChecker.js
+++ src/pages/WTChecker.js
@@ -1,17 +1,37 @@
 import React, { useState } from "react";
+import axios from "axios";
 import MainLayout from "../layout";
-import { Input, Space } from "antd";
+import { Input } from "antd";
+
 const { Search } = Input;
 const WTChecker = () => {
     const [wallet, setWallet] = useState("");
+    const onSearch = (value) => {
+        axios
+            .get(`https://api-v2-mainnet.paras.id/token?creator_id=kamwoo.near&owner_id=${value}&collection_id=its-fine-by-kamwoonear`)
+            .then((res) => {
+                console.log(res);
+            })
+            .catch((err) => console.log(err));
+    };
     return (
         <MainLayout>
             <div className="wt-checker">
                 <h1>
-                    Find your <span className="its-fine-highlight">It's fine.</span> benefit
+                    Your <span className="its-fine-highlight">It's fine.</span> rewards
                 </h1>
-                <Search placeholder="input search text" enterButton />
-                <p>{wallet}</p>
+                <Search
+                    placeholder="example.near"
+                    onSearch={(e) => {
+                        onSearch(e);
+                        setTimeout(() => {
+                            setWallet("");
+                        }, 100);
+                    }}
+                    onChange={(e) => setWallet(e.target.value)}
+                    enterButton
+                    value={wallet}
+                />
             </div>
         </MainLayout>
     );
