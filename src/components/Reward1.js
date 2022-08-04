a0feaef591ebdb8e5d89a1e52bb10e07e30c1f57
nitipon <nitipon.apaisri@gmail.com>
2022-08-04 23:46:18 +0200

    fixed design bug

--- src/components/Reward1.js
+++ src/components/Reward1.js
@@ -1,10 +1,31 @@
+import { Col, Row } from "antd";
+import React, { useEffect, useState } from "react";
+
 const Reward1 = () => {
+    const [width, setWidth] = useState(window.innerWidth);
+    const updateWindowSize = () => {
+        setWidth(window.innerWidth);
+    };
+    useEffect(() => {
+        window.addEventListener("resize", updateWindowSize);
+    });
     return (
-        <div className="reward1-container">
-            <div className="result-wrapper">
-                <div className="reward-img" style={{ backgroundImage: `url(./img/default-character.jpeg)` }}></div>
-                <h4>A Default Characer</h4>
-            </div>
+        <div className="reward2-container">
+            {width <= 480 ? (
+                <div className="responsive-vertical">
+                    <div className="result-wrapper">
+                        <div className="reward-img" style={{ backgroundImage: `url(./img/default-character.jpeg)` }}></div>
+                        <h4>A Default Characer</h4>
+                    </div>
+                </div>
+            ) : (
+                <Row gutter={16} justify="center">
+                    <Col className="result-wrapper" span={8}>
+                        <div className="reward-img" style={{ backgroundImage: `url(./img/default-character.jpeg)` }}></div>
+                        <h4>A Default Characer</h4>
+                    </Col>
+                </Row>
+            )}
         </div>
     );
 };
