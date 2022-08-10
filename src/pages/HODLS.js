85f9188e17900709b6757ccd3ad9975152c56033
nitipon <nitipon.apaisri@gmail.com>
2022-08-10 21:53:16 +0200

    removed strict mode

--- src/pages/HODLS.js
+++ src/pages/HODLS.js
@@ -1,38 +1,46 @@
-import { Button } from "antd";
+import { Button, Table } from "antd";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { positiveTokens } from "../assets/positiveATK";
 import MainLayout from "../layout";
 
 const HOLDS = () => {
-    const [owners, setOwners] = useState([]);
+    const [holders, setHolders] = useState([]);
     const [ownTokens, setOwnTokens] = useState([]);
     const [ownPositiveTokens, setOwnPositiveTokens] = useState([]);
     const [owner, setOwner] = useState({ wallet: "", reward: "", bonus: false });
     const [ownerId, setOwnerId] = useState("");
     useEffect(() => {
-        for (let i = 0; i < ownTokens.length; i++) {
-            const findOwnPositiveTokens = positiveTokens.find((positiveToken) => positiveToken === ownTokens[i].token_series_id);
-            if (findOwnPositiveTokens !== undefined) {
-                setOwnPositiveTokens((token) => [...token, findOwnPositiveTokens]);
-            }
-        }
-    }, [ownTokens]);
-    useEffect(() => {
-        setTimeout(() => {
-            if (ownTokens.length >= 2 && ownPositiveTokens.length === 0) {
-                setOwners((owner) => [...owner, { wallet: ownTokens[0].owner_id, reward: "BASE" }]);
-            }
-            if (ownTokens.length >= 2 && ownPositiveTokens.length === 1) {
-                setOwners((owner) => [...owner, { wallet: ownTokens[0].owner_id, reward: "MS" }]);
-            }
-        }, 100);
-    }, [ownTokens, ownPositiveTokens]);
-    useEffect(() => {
-        if (owners.length >= 2) {
-            console.log([...new Set(owners)]);
-        }
-    });
+        console.log("hi");
+        fetch();
+    }, []);
+    const columns = [
+        {
+            title: "Holder",
+            dataIndex: "holder",
+            key: "hold",
+            render: (text) => <p>{text}</p>,
+        },
+        {
+            title: "ATK",
+            dataIndex: "holder",
+            key: "hold",
+            render: (text) => <p>{text}</p>,
+        },
+    ];
+    const fetch = () => {
+        axios
+            .get("https://api-v2-mainnet.paras.id/collection-stats?collection_id=its-fine-by-kamwoonear")
+            .then((res) => {
+                setHolders((holder) => [...holder, { holder: "nitipon-apaisri.near" }]);
+
+                // checkHolder("12lil.near");
+                // for (let i = 0; i < 2; i++) {
+                //     setHolders((holder) => [...holder, { holder: res.data.data.results.owner_ids[i] }]);
+                // }
+            })
+            .catch((err) => console.log(err));
+    };
     const checkHolder = (ownerId) => {
         axios
             .get(`https://api-v2-mainnet.paras.id/token?creator_id=kamwoo.near&owner_id=${ownerId}&collection_id=its-fine-by-kamwoonear`)
@@ -40,35 +48,21 @@ const HOLDS = () => {
                 if (res.data.data.results.length !== 0) {
                     res.data.data.results.forEach((r) => {
                         if (r.metadata.copies < 2) {
-                            setOwnTokens((token) => [...token, r]);
+                            console.log(res.data.data.results);
+                            // setHolders((token) => [...token, r]);
                         }
                     });
                 }
             })
             .catch((err) => console.log(err));
     };
-    const fetch = () => {
-        axios
-            .get("https://api-v2-mainnet.paras.id/collection-stats?collection_id=its-fine-by-kamwoonear")
-            .then((res) => {
-                setOwnTokens([]);
-                checkHolder("nitipon-apaisri.near");
-                setOwnTokens([]);
-                checkHolder("12lil.near");
-                // for (let i = 0; i < 2; i++) {
-                //     checkHolder(res.data.data.results.owner_ids[i]);
-                // }
-            })
-            .catch((err) => console.log(err));
-    };
+
     return (
         <MainLayout>
             <div className="hold-container">
                 <p>HOLD</p>
-                <>{/* <p>total owner: {Array(new Set(owners.length))}</p> */}</>
-                <Button onClick={fetch} type="primary">
-                    Click
-                </Button>
+                <p>{holders.length}</p>
+                {holders.length !== 0 && <Table columns={columns} dataSource={holders} rowKey={(row) => row.holder} />}
             </div>
         </MainLayout>
     );
